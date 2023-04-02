def70453ff97de2c70dd6d10ce292c545abb6416
Paul Gathondu <aulgondu@gmail.com>
2023-04-01 18:31:32 -0700

    update settings.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,7 @@
-"""mysite URL Configuration
+"""vitesite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
